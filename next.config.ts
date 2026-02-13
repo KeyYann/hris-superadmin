61dc4a3616eb8ded726c922d7534ba243e47c776
KeyYann <kyannedgar04@gmail.com>
2026-02-13 08:28:36 +0800

    Update admin management, dashboard, events, history, time-off credits, trash pages and notification context

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: 'export',
-  basePath: isProd ? '/hris-superadmin' : '',
-  assetPrefix: isProd ? '/hris-superadmin' : '',
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
 };
 
 export default nextConfig;
